a2055bfc84f259ab898448d99629036067bb0f07
nankuF <fireloki87@gmail.com>
2022-03-31 20:43:17 +0300

    rename functions; moved path to variable.

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import os
 
 from PIL import Image
 
+PIXEL = 50
+
+PATH_TO_AVATARS_DIR = './avatars/'
+PATH_TO_MODIFIED_PICTURES_DIR = './modified_pictures/'
+PATH_TO_NEED_MODIFIED_DIR = './need_modified/'
+
 
 def cut_left(color, size):
     return color.crop(box=(size, 0, color.width, color.height))
@@ -27,12 +33,9 @@ def shift_picture_right(color, size):
     return Image.blend(picture_right, picture_middle, alpha=0.3)
 
 
-PIXEL = 50
-
-
-def get_modified_picture(picture):
+def create_modified_picture(picture):
     """Только jpg картинки"""
-    rgb_image = Image.open(f'./need_modified/{picture}')
+    rgb_image = Image.open(f'{PATH_TO_NEED_MODIFIED_DIR}{picture}')
     red, blue, green = rgb_image.split()
 
     red_left = shift_picture_left(red, PIXEL)
@@ -40,18 +43,18 @@ def get_modified_picture(picture):
     green_middle = cut_left_and_right(green, PIXEL)
 
     new_picture = Image.merge('RGB', (red_left, green_middle, blue_right))
-    new_picture.save(f'./modified_pictures/new_{picture}')
+    new_picture.save(f'{PATH_TO_MODIFIED_PICTURES_DIR}new_{picture}')
 
 
-def get_avatar(picture):
+def create_avatar(picture):
     """Только jpg картинки"""
-    avatar = Image.open(f'./modified_pictures/new_{picture}')
+    avatar = Image.open(f'{PATH_TO_MODIFIED_PICTURES_DIR}new_{picture}')
     avatar.thumbnail((80, 80))
-    avatar.save(f'./avatars/avatar_{picture}')
+    avatar.save(f'{PATH_TO_AVATARS_DIR}avatar_{picture}')
 
 
 def get_jpg():
-    files = os.listdir('./need_modified')
+    files = os.listdir(f'{PATH_TO_NEED_MODIFIED_DIR}')
     pictures = []
     for file in files:
         if '.jpg' in file:
@@ -62,8 +65,8 @@ def get_jpg():
 def main():
     pictures = get_jpg()
     for picture in pictures:
-        get_modified_picture(picture)
-        get_avatar(picture)
+        create_modified_picture(picture)
+        create_avatar(picture)
 
 
 if __name__ == '__main__':
