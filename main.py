2a00b8a256714cf79cdc247b8342ced3bbfc8c08
nankuF <fireloki87@gmail.com>
2022-03-31 21:18:31 +0300

    delete functions

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 
 from PIL import Image
-from PIL.JpegImagePlugin import JpegImageFile
 
 PIXEL = 50
 
@@ -10,32 +9,23 @@ PATH_TO_MODIFIED_PICTURES_DIR = './modified_pictures/'
 PATH_TO_NEED_MODIFIED_DIR = './need_modified/'
 
 
-def cut_left(color, size):
-    return color.crop(box=(size, 0, color.width, color.height))
-
-
-def cut_right(color, size):
-    return color.crop(box=(0, 0, color.width - size, color.height))
-
-
 def cut_left_and_right(color, size):
     return color.crop(box=(size // 2, 0, color.width - size // 2, color.height))
 
 
 def shift_picture_left(color, size):
-    picture_left = cut_left(color, size)
+    picture_left = color.crop(box=(size, 0, color.width, color.height))
     picture_middle = cut_left_and_right(color, size)
     return Image.blend(picture_left, picture_middle, alpha=0.3)
 
 
 def shift_picture_right(color, size):
-    picture_right = cut_right(color, size)
+    picture_right = color.crop(box=(0, 0, color.width - size, color.height))
     picture_middle = cut_left_and_right(color, size)
     return Image.blend(picture_right, picture_middle, alpha=0.3)
 
 
 def create_modified_picture(picture):
-    """Только jpg картинки"""
     rgb_image = Image.open(f'{PATH_TO_NEED_MODIFIED_DIR}{picture}')
     red, blue, green = rgb_image.split()
 
@@ -48,7 +38,6 @@ def create_modified_picture(picture):
 
 
 def create_avatar(picture):
-    """Только jpg картинки"""
     avatar = Image.open(f'{PATH_TO_MODIFIED_PICTURES_DIR}new_{picture}')
     avatar.thumbnail((80, 80))
     avatar.save(f'{PATH_TO_AVATARS_DIR}avatar_{picture}')
