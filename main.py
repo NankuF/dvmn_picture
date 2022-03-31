2edbb03f9a0839bed3112ddf33096c3dd522c8c1
nankuF <fireloki87@gmail.com>
2022-03-31 21:26:44 +0300

    renamed function

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ PATH_TO_MODIFIED_PICTURES_DIR = './modified_pictures/'
 PATH_TO_NEED_MODIFIED_DIR = './need_modified/'
 
 
-def cut_left_and_right(color, size):
+def cut_color_left_and_right(color, size):
     return color.crop(box=(size // 2, 0, color.width - size // 2, color.height))
 
 
 def shift_picture_left(color, size):
     picture_left = color.crop(box=(size, 0, color.width, color.height))
-    picture_middle = cut_left_and_right(color, size)
+    picture_middle = cut_color_left_and_right(color, size)
     return Image.blend(picture_left, picture_middle, alpha=0.3)
 
 
 def shift_picture_right(color, size):
     picture_right = color.crop(box=(0, 0, color.width - size, color.height))
-    picture_middle = cut_left_and_right(color, size)
+    picture_middle = cut_color_left_and_right(color, size)
     return Image.blend(picture_right, picture_middle, alpha=0.3)
 
 
@@ -31,7 +31,7 @@ def create_modified_picture(picture):
 
     red_left = shift_picture_left(red, PIXEL)
     blue_right = shift_picture_right(blue, PIXEL)
-    green_middle = cut_left_and_right(green, PIXEL)
+    green_middle = cut_color_left_and_right(green, PIXEL)
 
     new_picture = Image.merge('RGB', (red_left, green_middle, blue_right))
     new_picture.save(f'{PATH_TO_MODIFIED_PICTURES_DIR}new_{picture}')
